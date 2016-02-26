706ff18fc42a6a5e33623d9c244accf70b314898
jaime <garciadiazjaime@gmail.com>
2016-02-26 15:29:48 -0600

    setup place app

---
+++ project/place/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from .models import Category, Place, Link
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+
+class PlaceAdmin(admin.ModelAdmin):
+	list_display = ('id', 'code', 'name', 'latitud', 'longitude')
+	search_fields = ['code', 'name']
+
+class LinkAdmin(admin.ModelAdmin):
+	list_display = ('id', 'type', 'url', 'place')
+	list_filter = ('type',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Place, PlaceAdmin)
+admin.site.register(Link, LinkAdmin)
---
+++ project/place/models.py
@@ -0,0 +1,34 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __unicode__(self):
+        return "%s" % (self.name)
+
+class Place(models.Model):
+    name = models.CharField(max_length=120)
+    latitud = models.CharField(max_length=120)
+    longitude = models.CharField(max_length=120)
+    code = models.CharField(max_length=120, blank=True, null=True)
+    categories = models.ManyToManyField(Category, blank=True, null=True)
+
+    def __unicode__(self):
+		return "%s - %s" % (self.code, self.name)
+
+class Link(models.Model):
+    LINK_TYPES =(
+        (1, 'WEBSITE'),
+        (2, 'GMAPS'),
+        (3, 'FACEBOOK'),
+        (4, 'YELP'),
+    )
+    type = models.IntegerField(choices=LINK_TYPES)
+    url = models.URLField(max_length=500)
+    place = models.ForeignKey(Place)
+
+    def __unicode__(self):
+        return "%s" % self.category
